eb82777070396a2fdd84aa49ddd7a202e90e26de
zakirya592 <muhammadzakirya592@gmail.com>
2026-08-26 12:53:32 +0500

    Updated

--- src/Pages/Expenses/Expenses.jsx
+++ src/Pages/Expenses/Expenses.jsx
@@ -212,6 +212,7 @@ function Expenses() {
             </div>
           </div>
 
+
           <div className="rounded-xl border border-slate-200 bg-white p-5 shadow-sm">
             <div className="flex items-center justify-between">
               <div>
